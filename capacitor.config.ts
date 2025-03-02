340363a1326c09085c9c4df689dcaded2f180ba8
Yasya4444 <lukonina04an@mail.ru>
2025-03-02 12:15:40 +0400

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.catbreed.app',
+  appName: 'CatBreedFinal',
+  webDir: 'build'
+};
+
+export default config;
